5920a44c1e482472ff991856b7efabcec366b5b1
Liam Hepper <liamhepper1@gmail.com>
2023-06-21 15:48:08 +0100

    base template hookup

--- CoderKai/main_app/urls.py
+++ CoderKai/main_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.welcome_page, name="welcome-page")
+    path("", views.welcome_page, name="welcome-page"),
+    path("user-profile", views.user_profile, name="user-profile")
 ]
--- CoderKai/main_app/views.py
+++ CoderKai/main_app/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def welcome_page(request):
     return render(request, "./main_app/welcome_page.html")
+
+
+def user_profile(request):
+    return render(request, "./main_app/user_profile.html")
