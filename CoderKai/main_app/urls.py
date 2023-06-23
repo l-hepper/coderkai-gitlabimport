27be4aae7b06bd2475c30ed8fbc38cd3dd706b03
Liam Hepper <liamhepper1@gmail.com>
2023-06-23 12:21:30 +0100

    all html templates placed within application folder for now

--- CoderKai/main_app/urls.py
+++ CoderKai/main_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.welcome_page, name="welcome-page"),
-    path("user-profile", views.user_profile, name="user-profile")
+    path("user-profile", views.user_profile, name="user-profile"),
+    path("posts", views.posts, name="posts"),
+    path("about", views.about, name="about"),
+    path("<str:attemptedURL>", views.raise_404_error, name="404Error")
 ]
--- CoderKai/main_app/views.py
+++ CoderKai/main_app/views.py
@@ -1,11 +1,33 @@
+from django.http import Http404
 from django.shortcuts import render
 
 
 # Create your views here.
 
+
 def welcome_page(request):
-    return render(request, "./main_app/welcome_page.html")
+    return render(request, "./main_app/welcome_page.html", {
+        "page_title": "Welcome!"
+    })
 
 
 def user_profile(request):
-    return render(request, "./main_app/user_profile.html")
+    return render(request, "./main_app/user_profile.html", {
+        "page_title": "UserProfile"
+    })
+
+
+def posts(request):
+    return render(request, "./main_app/posts.html", {
+        "page_title": "Posts"
+    })
+
+
+def about(request):
+    return render(request, "./main_app/about.html", {
+        "page_title": "About"
+    })
+
+
+def raise_404_error(request, attemptedURL):
+    raise Http404(f"This page does not exist on CoderKai")
