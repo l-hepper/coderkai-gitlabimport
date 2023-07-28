c193deba856cd9780c656c452173f8ad0d9a9149
Liam Hepper <liamhepper1@gmail.com>
2023-07-28 17:02:33 +0100

    postgres integration

--- CoderKai/CoderKai/settings.py
+++ CoderKai/CoderKai/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'CoderKai.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coderkaipro',
+        'USER': 'postgres',
+        'PASSWORD': 'whatever123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- CoderKai/main_app/models.py
+++ CoderKai/main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class User(models.Model):
+    username = models.CharField(max_length=16)
+    email = models.CharField(max_length=50)
+    code = models.IntegerField()
+    
