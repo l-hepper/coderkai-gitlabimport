039dc4c7a8642041c252141f590a2cd468027e7e
Liam Hepper <liamhepper1@gmail.com>
2023-08-28 11:13:19 +0100

    layout changes and further preparation for deployment

--- CoderKai/CoderKai/settings.py
+++ CoderKai/CoderKai/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 
 
@@ -155,11 +155,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
-
-django_on_heroku.settings(locals())
 
 LOGIN_URL = reverse_lazy("login")
 LOGIN_REDIRECT_URL = "welcome_page"
